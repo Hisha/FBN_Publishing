90921f40b8aeaa149e3c997c04a37bb6dd832b04
Kevin Smith <vndinmchine@gmail.com>
2025-07-29 19:47:09 -0400

    Delete start_flux_service.sh

--- start_flux_service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/smithkt/FBN_Publishing/FBNP_env/bin/activate
-cd /home/smithkt/FBN_Publishing
-
-# Start FastAPI server in background
-/home/smithkt/FBN_Publishing/FBNP_env/bin/uvicorn fbnp_api:app --host 0.0.0.0 --port 8000 &
-
-# Start worker pool (waits)
-python start_workers.py
