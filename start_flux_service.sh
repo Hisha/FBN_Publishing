735f2382213462c1d5e0d2b6f9721d76195e7f4f
Kevin Smith <vndinmchine@gmail.com>
2025-07-29 15:46:47 -0400

    Update start_flux_service.sh

--- start_flux_service.sh
+++ start_flux_service.sh
@@ -3,7 +3,7 @@ source /home/smithkt/FBN_Publishing/FBNP_env/bin/activate
 cd /home/smithkt/FBN_Publishing
 
 # Start FastAPI server in background
-/home/smithkt/FBN_Publishing/FBNP_env/bin/uvicorn flux_api:app --host 0.0.0.0 --port 8000 &
+/home/smithkt/FBN_Publishing/FBNP_env/bin/uvicorn fbnp_api:app --host 0.0.0.0 --port 8000 &
 
 # Start worker pool (waits)
 python start_workers.py
