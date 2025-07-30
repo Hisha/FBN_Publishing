a48293aa7481c12e4ec71843e6e33d798af58126
Kevin Smith <vndinmchine@gmail.com>
2025-07-29 20:39:01 -0400

    Create start_fbnp_services.sh

---
+++ start_fbnp_services.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /home/smithkt/FBN_publishing/FBNP_env/bin/activate
+cd /home/smithkt/FBN_publishing
+
+# Start FastAPI server in background
+/home/smithkt/FBN_publishing/FBNP_env/bin/uvicorn fbnp_api:app --host 0.0.0.0 --port 8000 &
+
+# Start worker pool (waits)
+python start_workers.py
