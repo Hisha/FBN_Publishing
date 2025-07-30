921aa0c216396c8c46a4a9d28c8aac5d0bd3afdc
Kevin Smith <vndinmchine@gmail.com>
2025-07-29 22:57:20 -0400

    Update start_fbnp_services.sh

--- start_fbnp_services.sh
+++ start_fbnp_services.sh
@@ -3,7 +3,7 @@ source /home/smithkt/FBN_publishing/FBNP_env/bin/activate
 cd /home/smithkt/FBN_publishing
 
 # Start FastAPI server in background
-/home/smithkt/FBN_publishing/FBNP_env/bin/uvicorn fbnp_api:app --host 0.0.0.0 --port 8000 &
+/home/smithkt/FBN_publishing/FBNP_env/bin/uvicorn fbnp_api:app --host 0.0.0.0 --port 8000 --reload --log-level debug &
 
 # Start worker pool (waits)
 python start_workers.py
